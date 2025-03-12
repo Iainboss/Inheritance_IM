ecaae08672298ccb0ad79baf43cec94c314d565c
Iainboss <moylesiain@gmail.com>
2025-03-11 20:16:56 -0600

    family update

--- src/FamilyTree/FamilyTreeMain.java
+++ src/FamilyTree/FamilyTreeMain.java
@@ -23,7 +23,7 @@ person1.children.get(2).children.get(1).addChild(new Person("Zaiesha", "Canada")
 person1.children.get(2).children.get(1).addChild(new Person("Steve", "USA"));
 
 //Add Me + my children
-        person1.children.get(1).addChild( new Person ("Iain", "Canada"));
+        person1.children.get(1).addChild(new Person ("Iain", "Canada"));
         person1.children.get(1).children.get(0).addChild(new Person("Iain's kid1", "Canada"));
         person1.children.get(1).children.get(0).addChild(new Person("Iain's kid2", "Canada"));
         person1.children.get(1).children.get(0).children.get(0).addChild(new Person("Iain's grandson", "Canada"));
@@ -31,8 +31,10 @@ person1.children.get(2).children.get(1).addChild(new Person("Steve", "USA"));
 
         System.out.println(person1.children.get(1).children.get(0).children.get(0).children.get(0));
 
-    }
 
+        printFamily(person1,null);
+
+    }
     public static int countFamily(Person ptemp){
         int num = 0;
 
@@ -42,8 +44,22 @@ person1.children.get(2).children.get(1).addChild(new Person("Steve", "USA"));
     public static void printCanadians(Person ptemp){
 
     }
-public static void printFamily(){
-        
+public static void printFamily(Person p, Person parent){
+    // Base case: If the current person is null, return (stop recursion)
+    if (p == null) {
+        return;
+    }
+
+    // Check if the person has a parent (parent is null or not)
+    String parentName = parent == null ? "No Parent" : parent.name;  // Simple check for parent
+
+    // Print the current person's name and their parent's name (if they have one)
+    System.out.println(p.name + " (Parent: " + parentName + ")");
+
+    // Recursively traverse the children (go down the tree)
+    for (Person child : p.children) {
+        printFamily(child, p);  // Call the function for each child, passing the current person as the parent
+    }
 
 }
 
